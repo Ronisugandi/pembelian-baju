1314e1789a3e219ea9f05b49c9e21978fa8aa779
Ronisugandi <roniisugandi@gmail.com>
2025-07-11 16:48:21 +0700

    Perbaiki Procfile dan app.py untuk deployment

--- app.py
+++ app.py
@@ -136,6 +136,11 @@ def checkout(id):
 
     return render_template('checkout.html', product=product)
 
+import os
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
 
 if __name__ == '__main__':
     with app.app_context():
